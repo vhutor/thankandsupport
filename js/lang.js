8dcacaebb653c0fe99cc247e7a3baa27041725ae
Vlad Deputat <deputatwladek@gmail.com>
2022-09-12 23:46:00 +0300

    add english version

---
+++ js/lang.js
@@ -0,0 +1,33 @@
+import ua from "../locals/ua.json" assert {type: "json"};
+import en from "../locals/en.json" assert {type: "json"};
+
+let currentLang = ua;
+
+const switchLang = document.querySelector("#language-switcher");
+const langText = document.querySelectorAll("[data-lang]");
+
+const translate = (lang) => {
+  localStorage.setItem("lang", lang);
+  const source = lang === "ua" ? ua : en;
+  langText.forEach((el) => {
+    el.innerText = source[el.dataset.lang];
+  });
+
+  currentLang = lang;
+  // document.body.classList.toggle("lang-ua", lang === "ua");
+  // document.body.classList.toggle("lang-en", lang === "en");
+};
+
+switchLang.addEventListener("change", (e) => {
+  translate(e.target.value);
+});
+
+window.addEventListener("load", () => {
+  const lang = localStorage.getItem("lang");
+
+  if (lang) {
+    translate(lang);
+  }
+
+  switchLang.value = lang;
+});
